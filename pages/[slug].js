705f0ac477b71641bf569d7aa408304937d65993
Chris Padilla <cdpadilla42@gmail.com>
2022-06-09 09:15:48 -0500

    Adjust og images

--- pages/[slug].js
+++ pages/[slug].js
@@ -27,10 +27,10 @@ export default function Post({ post, morePosts, preview }) {
             <article className="mb-32">
               <Head>
                 <title>{post.title} | Chris Padilla</title>
-                <meta
+                {/* <meta
                   property="og:image"
                   content={post.ogImage?.url || '/assets/headshot.jpeg'}
-                />
+                /> */}
               </Head>
               <PostHeader
                 title={post.title}
