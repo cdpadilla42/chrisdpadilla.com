761fe9f59947a21f19cd913947706ab9e2e0bf6f
Chris Padilla <cdpadilla42@gmail.com>
2022-06-09 09:15:35 -0500

    Add twitter to footer

--- components/footer.js
+++ components/footer.js
@@ -1,21 +1,12 @@
-import {
-  DEV_TWITTER_URL,
-  GITHUB_URL,
-  INSTAGRAM_URL,
-  MUSIC_TWITTER_URL,
-} from '../lib/constants';
+import { MUSIC_TWITTER_URL, GITHUB_URL, INSTAGRAM_URL } from '../lib/constants';
 
 export default function Footer() {
   return (
     <footer>
       <span>© {new Date().getFullYear()} Chris Padilla</span>
       <br />
-      <a href={DEV_TWITTER_URL} target="_blank" rel="noopener noreferrer">
-        Dev Twitter
-      </a>{' '}
-      •{' '}
       <a href={MUSIC_TWITTER_URL} target="_blank" rel="noopener noreferrer">
-        Music Twitter
+        Twitter
       </a>{' '}
       •{' '}
       <a href={GITHUB_URL} target="_blank" rel="noopener noreferrer">
