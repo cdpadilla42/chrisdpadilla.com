319076a15d030f33f49075ac3ade6d7afd0517be
Chris Padilla <cdpadilla42@gmail.com>
2022-09-23 13:36:04 -0500

    Remove ogImage import

--- components/meta.js
+++ components/meta.js
@@ -1,6 +1,5 @@
 import Head from 'next/head';
 import { HOME_OG_IMAGE_URL } from '../lib/constants';
-import OGImage from './OGImage';
 
 export default function Meta({ customOGImage }) {
   const ogImage = customOGImage || HOME_OG_IMAGE_URL;
