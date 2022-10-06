1846d97f086bb28107f2ad46154cce510f6b89b4
Chris Padilla <cdpadilla42@gmail.com>
2022-10-06 11:01:22 -0500

    Playing the game update

--- pages/acnm.js
+++ pages/acnm.js
@@ -4,10 +4,9 @@ import Link from 'next/link';
 import React from 'react';
 import Header from '../components/header';
 import Layout from '../components/layout';
-import ACNMStartDisplay from  '../public/assets/projects/acnmfrontpage.png'
+import ACNMStartDisplay from '../public/assets/projects/acnmfrontpage.png';
 
 const About = () => {
-
   return (
     <Layout>
       <Head>
@@ -16,57 +15,137 @@ const About = () => {
       <Header />
       <h1>AC: New Murder </h1>
       <div className="center">
+        <Image src={ACNMStartDisplay} alt={`AC: New Mureder Start Page`} />
+      </div>
 
-      <Image
-        src={ACNMStartDisplay}
-        alt={`AC: New Mureder Start Page`}
-        />
-        </div>
-
-      
       <h2>I Made A Game!</h2>
-      <p><Link href="https://acnewmurder.com/play"><a target="_blank" rel="noopener noreferrer">AC: New Murder</a></Link> is a murder-mystery who-dun-it in the style of Phoenix Wright, featuring characters from Animal Crossing. The story and art are done by my sister Jenn. I developed the game for web browsers and built out all things software related.</p>
+      <p>
+        <Link href="https://acnewmurder.com/play">
+          <a target="_blank" rel="noopener noreferrer">
+            AC: New Murder
+          </a>
+        </Link>{' '}
+        is a murder-mystery who-dun-it in the style of Phoenix Wright, featuring
+        characters from Animal Crossing. The story and art are done by my sister
+        Jenn. I developed the game for web browsers and built out all things
+        software related.
+      </p>
 
-      <p>Oh, and I <Link href="https://letsgochris.bandcamp.com/album/ac-new-murder-soundtrack"><a target="_blank" rel="noopener noreferrer">made the music</a></Link>, too!</p>
+      <p>
+        Oh, and I{' '}
+        <Link href="https://letsgochris.bandcamp.com/album/ac-new-murder-soundtrack">
+          <a target="_blank" rel="noopener noreferrer">
+            made the music
+          </a>
+        </Link>
+        , too!
+      </p>
 
-      <p>2 years ago, my sister <Link href="https://www.jennpadilla.com/"><a target="_blank" rel="noopener noreferrer">Jenn</a></Link> approached me with an idea for a murder mystery game featuring some of the characters from Animal Crossing. I was looking for a fun new way to sharpen my software engineering skills, and I was won over by the silliness of the story!</p>
+      <p>
+        2 years ago, my sister{' '}
+        <Link href="https://www.jennpadilla.com/">
+          <a target="_blank" rel="noopener noreferrer">
+            Jenn
+          </a>
+        </Link>{' '}
+        approached me with an idea for a murder mystery game featuring some of
+        the characters from Animal Crossing. I was looking for a fun new way to
+        sharpen my software engineering skills, and I was won over by the
+        silliness of the story!
+      </p>
 
       <h2>The History</h2>
 
-      <p>What started out as a pretty simple web app for displaying text turned into a fully fledged visual novel.</p>
+      <p>
+        What started out as a pretty simple web app for displaying text turned
+        into a fully fledged visual novel.
+      </p>
 
       <p>And now you can play it!!</p>
 
       <h2>Playing the Game</h2>
 
-      <p>The game works in tandem with visiting Jenn's island in Animal Crossing: New Horizons. You can <Link href="https://acnewmurder.com/play"><a target="_blank" rel="noopener noreferrer">get started here</a></Link>!
-      
-      {/* If you don't have the game, or just want to go through the web app by itself, you can click here to unlock the items ahead of time. */}
-      
+      <p>
+        The game works in tandem with visiting Jenn's island in Animal Crossing:
+        New Horizons. Completing the game (and enjoying all the cool
+        customizations Jenn made in New Horizons) requires a Nintendo Switch and
+        the official Animal Crossing game.
+      </p>
+      <p>
+        If you don't have either, you can still work through the first act of
+        the game to get a feel for the web app!
+      </p>
+      <p>
+        You can{' '}
+        <Link href="https://acnewmurder.com/play">
+          <a target="_blank" rel="noopener noreferrer">
+            get started here
+          </a>
+        </Link>
+        !
+        {/* If you don't have the game, or just want to go through the web app by itself, you can click here to unlock the items ahead of time. */}
       </p>
 
       <h2>Tech Stack</h2>
 
       <p>
-      The game is a web app, so all you need to do is visit the site to get started.
-      </p><p>
-      For those curious, it was developed with the same tools I use for software engineering on the web: React, Redux, Node, MongoDB, and more. You can read about it in my <Link href="/acnmp"><a>blog posts</a></Link>.</p>
+        For those curious, the game was developed with the same tools I use for
+        software engineering on the web: React, Redux, Node, MongoDB, and more.
+        You can read about it in my{' '}
+        <Link href="/acnmp">
+          <a>blog posts</a>
+        </Link>
+        .
+      </p>
 
       <h2>Links</h2>
-      <p><Link href="https://acnewmurder.com/play"><a target="_blank" rel="noopener noreferrer">🕵️‍♂️ Play AC: New Murder</a></Link></p>
+      <p>
+        <Link href="https://acnewmurder.com/play">
+          <a target="_blank" rel="noopener noreferrer">
+            🕵️‍♂️ Play AC: New Murder
+          </a>
+        </Link>
+      </p>
 
-      <p><Link href="https://acnewmurder.com/"><a target="_blank" rel="noopener noreferrer">🔍 Visit the Official Site</a></Link></p>
+      <p>
+        <Link href="https://acnewmurder.com/">
+          <a target="_blank" rel="noopener noreferrer">
+            🔍 Visit the Official Site
+          </a>
+        </Link>
+      </p>
 
-      <p><Link href="https://letsgochris.bandcamp.com/album/ac-new-murder-soundtrack"><a target="_blank" rel="noopener noreferrer">🎶 Listen to the Soundtrack</a></Link></p>
+      <p>
+        <Link href="https://letsgochris.bandcamp.com/album/ac-new-murder-soundtrack">
+          <a target="_blank" rel="noopener noreferrer">
+            🎶 Listen to the Soundtrack
+          </a>
+        </Link>
+      </p>
 
       <p>🧑‍💻 Read more about the project on my blog:</p>
       <ul>
-        <li><Link href="/acnmp"><a>Technical Overview</a></Link></li>
-        <li><Link href="/acnmfe"><a>Developing a Game in React</a></Link></li>
-        <li><Link href="/acnmbe"><a>Using Sanity as a Level Maker</a></Link></li>
-        <li><Link href="/acnmpm"><a>Project Management for Game Development</a></Link></li>
+        <li>
+          <Link href="/acnmp">
+            <a>Technical Overview</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/acnmfe">
+            <a>Developing a Game in React</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/acnmbe">
+            <a>Using Sanity as a Level Maker</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/acnmpm">
+            <a>Project Management for Game Development</a>
+          </Link>
+        </li>
       </ul>
-
     </Layout>
   );
 };
