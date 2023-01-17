9b83d26a375530ce53f8305f25c074a0698db78a
Chris Padilla <cdpadilla42@gmail.com>
2023-01-17 12:09:28 -0600

    Verify Links exclude insta

--- cypress/e2e/verifyLinks.cy.js
+++ cypress/e2e/verifyLinks.cy.js
@@ -2,6 +2,8 @@ import { BASE_URL, BASE_URL_DEV } from '../../lib/constants';
 
 const baseUrl = process.env.NODE_ENV === 'production' ? BASE_URL : BASE_URL_DEV;
 
+const excludedLinks = ['bandcamp', 'linkedin', 'instagram'];
+
 describe('Test Broken Links', () => {
   it('Loads the landing page', () => {
     cy.request(baseUrl).then((res) => {
@@ -14,8 +16,13 @@ describe('Test Broken Links', () => {
     cy.visit(baseUrl);
     cy.get("a:not([href*='mailto:'])").each((linkElm) => {
       const linkPath = linkElm.attr('href');
-      const shouldExclude =
-        linkPath.includes('bandcamp') || linkPath.includes('linkedin');
+      const shouldExclude = excludedLinks.reduce((p, c) => {
+        if (p || linkPath.includes(c)) {
+          return true;
+        }
+        return false;
+      }, false);
+
       if (linkPath.length > 0 && !shouldExclude) {
         cy.request(linkPath).then((res) => {
           expect(res.status).to.eq(200);
