d7a53bcbb597456f0dc99654fd9fa15b055a051b
Chris Padilla <cdpadilla42@gmail.com>
2023-02-24 14:12:17 -0600

    Add full sitemap. Update robots.txt

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,23 +1,40 @@
 import { getAlbums, getAllPages, getAllPosts } from '../lib/api';
 import { BASE_URL } from '../lib/constants';
-import { getPagesFromDirectory } from '../lib/markdownAccess';
-
-const EXTERNAL_DATA_URL = 'https://jsonplaceholder.typicode.com/posts';
 
 function generateSiteMap(slugs) {
   return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
      <!--Manual URL's-->
      <url>
-       <loc>${BASE_URL}}</loc>
+       <loc>${BASE_URL}</loc>
+       <priority>1</priority>
+       <changefreq>daily</changefreq>
      </url>
      ${slugs
        .map((slug) => {
-         return `
-       <url>
-           <loc>${`${BASE_URL}/${slug}`}</loc>
-       </url>
-     `;
+         if (slug === 'blog') {
+           return `
+            <url>
+              <loc>${`${BASE_URL}/${slug}`}</loc>
+              <priority>0.9</priority>
+              <changefreq>daily</changefreq>
+            </url>
+          `;
+         } else if (slug === 'now' || slug === 'music') {
+           return `
+             <url>
+               <loc>${`${BASE_URL}/${slug}`}</loc>
+               <priority>0.9</priority>
+               <changefreq>monthly</changefreq>
+             </url>
+           `;
+         } else {
+           return `
+            <url>
+              <loc>${`${BASE_URL}/${slug}`}</loc>
+            </url>
+          `;
+         }
        })
        .join('')}
    </urlset>
@@ -31,10 +48,13 @@ function SiteMap() {
 export async function getServerSideProps({ res }) {
   // We make an API call to gather the URLs for our site
   const pages = getAllPages();
-  console.log(pages);
   const posts = getAllPosts(['slug']);
   const albums = getAlbums();
-  const slugs = [...albums, ...posts].map((contentObj) => contentObj.slug);
+  const dynamicSlugs = [...albums, ...posts].map(
+    (contentObj) => contentObj.slug
+  );
+
+  const slugs = [...pages, ...dynamicSlugs];
 
   // const request = await fetch(EXTERNAL_DATA_URL);
   // const posts = await request.json();
