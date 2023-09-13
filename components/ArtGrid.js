5c4b9eb68becf8518869e152e524f867738538ff
Chris Padilla <cdpadilla42@gmail.com>
2023-09-13 15:37:20 -0500

    Show only one image on mobile

--- components/ArtGrid.js
+++ components/ArtGrid.js
@@ -4,17 +4,19 @@ import React from 'react';
 const ArtGrid = ({ images }) => {
   return (
     <>
-      <section className="music_display">
-        {images.map((image) => (
-          <article key={image.src}>
+      <section className="art_display">
+        {images.map((image, i) => (
+          <article key={image.src} className={`${i > 0 ? 'hideonmobile' : ''}`}>
             <Link href={`/${image.slug}`}>
               <a data-test="musicGridLink">
                 <div
+                  className={`artgridimage`}
                   style={{
                     height: '250px',
                     width: '250px',
                     background: 'transparent no-repeat center',
                     'background-size': 'cover',
+                    margin: 'auto',
                     backgroundImage: `url('${image.src}')`,
                   }}
                 />
