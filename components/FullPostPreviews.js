225edffb69636274ed0b2bd6f811de29508ceceb
Chris Padilla <cdpadilla42@gmail.com>
2023-09-13 15:08:39 -0500

    Create Full Post Preview Component.

---
+++ components/FullPostPreviews.js
@@ -0,0 +1,61 @@
+import { useRouter } from 'next/router';
+import React from 'react';
+import Link from 'next/link';
+import queryString from 'query-string';
+import PostHeader from './post-header';
+import PostBody from './post-body';
+
+const FullPostPreviews = ({ posts, count }) => {
+  const router = useRouter();
+  const query = { ...router.query };
+  const currentPage = parseInt(query.p) || 1;
+  const lastPage = Math.ceil(count / 5);
+
+  return (
+    <div>
+      <ul className="bloglist">
+        {posts.length > 0 &&
+          posts.map((post) => (
+            <>
+              <PostHeader
+                title={post.title}
+                coverImage={post.coverImage}
+                date={post.date}
+                tags={post.tags}
+                slug={post.slug}
+              />
+              <PostBody content={post.content} />
+            </>
+          ))}
+      </ul>
+      <div className="pagination-controller" style={{ display: 'flex' }}>
+        <div className="left" style={{ flex: '1' }}>
+          {currentPage !== 1 && (
+            <Link
+              href={`${router.pathname}?${queryString.stringify({
+                ...query,
+                p: parseInt(query.p) - 1,
+              })}`}
+            >
+              Back
+            </Link>
+          )}
+        </div>
+        <div className="right" style={{ flex: '1', textAlign: 'right' }}>
+          {currentPage < lastPage && (
+            <Link
+              href={`${router.pathname}?${queryString.stringify({
+                ...query,
+                p: parseInt(query.p || 1) + 1,
+              })}`}
+            >
+              Next
+            </Link>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FullPostPreviews;
