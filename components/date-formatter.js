cc50cc92c178f3808b61dfd677b4d4b8f868b03d
Chris Padilla <cdpadilla42@gmail.com>
2024-06-30 15:22:17 -0500

    Durrable date formatter

--- components/date-formatter.js
+++ components/date-formatter.js
@@ -2,5 +2,15 @@ import { parseISO, format } from 'date-fns';
 
 export default function DateFormatter({ dateString }) {
   const date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, 'MM /	d / yyyy')}</time>;
+  let formattedDate = '';
+  try {
+    formattedDate = format(date, 'MM /	d / yyyy')
+  } catch (e) {
+    console.log(e);
+    console.log("DATE ERROR:")
+    console.log(dateString);
+  }
+  if(formattedDate) {
+    return <time dateTime={dateString}>{format(date, 'MM /	d / yyyy')}</time>;
+  }
 }
