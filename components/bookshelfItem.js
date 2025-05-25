2f1601a6c9e5a8cdfc0b0b7de7751dc453b45546
Chris Padilla <cdpadilla42@gmail.com>
2025-05-24 22:53:13 -0600

    what it is bookshelf

--- components/bookshelfItem.js
+++ components/bookshelfItem.js
@@ -3,46 +3,60 @@ import Markdown from 'markdown-to-jsx';
 import { capitalizeFirstLetter } from '../lib/util';
 import NextLink from './NextLink';
 
-const BookshelfItem = ({book}) => {
+const BookshelfItem = ({ book }) => {
+  const renderPostLinks = () => {
+    return book.postLinks.map((linkObj) => {
+      return (
+        <>
+          <li>
+            <NextLink href={linkObj.slug}>{linkObj.title}</NextLink>{' '}
+          </li>
+        </>
+      );
+    });
+  };
 
-	const renderPostLinks = () => {
-		return book.postLinks.map((linkObj) => {
-				return (
-					<>
-						<li><NextLink href={linkObj.slug}>{linkObj.title}</NextLink> </li>
-					</>
-				)
-		})
-	}
-
-	return (
-		<article key={book.name}>
-			<figure className='bookshelf_image_wrapper'>
-				<img src={book.image} className='bookshelf_image'/>
-			</figure>
-			<h3 id={book.slug}>{book.name} – By {book.author}</h3>
-			<small>Date Read: <strong>{book.date}</strong> | Status: <strong>{capitalizeFirstLetter(book.progress)}</strong>{book.rating && <> | Rating: <strong>{book.rating}/10</strong></>}{book.purchase && <> | <strong>
-				<NextLink href={book.purchase} >Purchase</NextLink>
-			</strong></>}</small>
-			{book.desc && (
-				<p>
-					 <Markdown>
-						{book.desc}
-      		</Markdown>
-				</p>
-			)}
-			{book.postLinks && (
-				<>
-				<strong>Posts referencing this book:</strong>
-				<ul>
-
-					{renderPostLinks()}
-				</ul>
-				</>
-			)}
-			<div className="clearfix" />
-		</article>
-	);
+  return (
+    <article key={book.name}>
+      <figure className="bookshelf_image_wrapper">
+        <img src={book.image} className="bookshelf_image" />
+      </figure>
+      <h3 id={book.slug}>
+        {book.name} – by {book.author}
+      </h3>
+      <small>
+        Date Read: <strong>{book.date}</strong> | Status:{' '}
+        <strong>{capitalizeFirstLetter(book.progress)}</strong>
+        {book.rating && (
+          <>
+            {' '}
+            | Rating: <strong>{book.rating}/10</strong>
+          </>
+        )}
+        {book.purchase && (
+          <>
+            {' '}
+            |{' '}
+            <strong>
+              <NextLink href={book.purchase}>Purchase</NextLink>
+            </strong>
+          </>
+        )}
+      </small>
+      {book.desc && (
+        <p>
+          <Markdown>{book.desc}</Markdown>
+        </p>
+      )}
+      {book.postLinks && (
+        <>
+          <strong>Posts referencing this book:</strong>
+          <ul>{renderPostLinks()}</ul>
+        </>
+      )}
+      <div className="clearfix" />
+    </article>
+  );
 };
 
 export default BookshelfItem;
